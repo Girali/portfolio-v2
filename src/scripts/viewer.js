4d64dc9dc60bb0a97344a22be3a29014c684f398
Girali <thegirali@wp.pl>
2025-02-13 00:16:46 +0100

    Image viewer added

---
+++ src/scripts/viewer.js
@@ -0,0 +1,43 @@
+﻿const imgViewerWrapper = document.getElementById("img-viewer");
+const imgViewerContainerWrapper = document.getElementById("portfolio-image-viewer");
+
+function setupViewers(){
+    document.querySelectorAll('.viewer').forEach(div => {
+        div.addEventListener("click", (e) => {
+            onViewerClick(div);
+        })
+    });
+}
+
+function onViewerClick(div) {
+    imgViewerContainerWrapper.classList.add('opaque');
+    imgViewerContainerWrapper.classList.remove('transparent');
+    imgViewerContainerWrapper.classList.remove('overlay-no-pointer-events');
+
+    const imgSrc = div.getAttribute('toView');
+    imgViewerWrapper.src = imgSrc;
+}
+
+function onQuitViewer(){
+    imgViewerContainerWrapper.classList.add('transparent');
+    imgViewerContainerWrapper.classList.remove('opaque');
+    imgViewerContainerWrapper.classList.add('overlay-no-pointer-events');
+}
+
+function notifyController() {
+    anime({
+        targets: '#notify-controller',
+        scale: ["80%", "130%"],
+        opacity: [0, 1, 0],
+        duration: 1000,
+        easing: 'easeInQuad',
+    });
+}
+
+imgViewerContainerWrapper.addEventListener("click", (e) => {
+    onQuitViewer();
+});
+
+document.getElementById("viewer-notify-controller").addEventListener("click", notifyController);
+
+setupViewers();
